442182ccbf4a8b2ff6842321586524031a2e3986
Matthew Kweon <kweon.matt@gmail.com>
2022-09-16 22:22:33 +0000

    saving progress

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var $addEntry = document.querySelector('.add-entry');
 var $modalContainer = document.querySelector('.modal-container');
 var $addEntryForm = document.querySelector('.add-entry-form');
-var $weekdayBoxesContainer = document.querySelector('.weekday-boxes-containers');
+var $weekdayBoxesContainer = document.querySelector('.weekday-boxes-container');
 var $weekdayBoxes = document.querySelectorAll('.column-seventh');
+var $span = document.querySelector('span');
 
 $addEntry.addEventListener('click', function (event) {
   $modalContainer.className = 'modal-container';
@@ -12,4 +13,16 @@ $addEntryForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
   $modalContainer.className = 'modal-conatiner hidden';
+});
+
+$weekdayBoxesContainer.addEventListener('click', function (event) {
+  $weekdayBoxes.className = 'column-seventh';
+  if (event.target.matches('.column-seventh')) {
+    var weekdayClicked = event.target.getAttribute('data-day');
+    $span.textContent = weekdayClicked;
+    for (var i = 0; i < $weekdayBoxes.length; i++) {
+      $weekdayBoxes[i].className = 'column-seventh';
+    }
+    event.target.className = 'column-seventh chosen-day';
+  }
 });
