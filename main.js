85376376e843907615dcb781f911fa11a97d719e
Jianni Cariaga <jiannicariaga@gmail.com>
2022-09-16 21:24:42 +0000

    Saving progress.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var $addEntry = document.querySelector('.add-entry');
 var $modalContainer = document.querySelector('.modal-container');
 var $addEntryForm = document.querySelector('.add-entry-form');
+var $weekdayBoxesContainer = document.querySelector('.weekday-boxes-containers');
+var $weekdayBoxes = document.querySelectorAll('.column-seventh');
 
 $addEntry.addEventListener('click', function (event) {
   $modalContainer.className = 'modal-container';
