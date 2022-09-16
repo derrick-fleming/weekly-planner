961c15eac82d5de579d6207f75151cb9ee99b2e1
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-16 23:00:33 +0000

    Worked on data model.

--- main.js
+++ main.js
@@ -5,12 +5,59 @@ var $weekdayBoxesContainer = document.querySelector('.weekday-boxes-container');
 var $weekdayBoxes = document.querySelectorAll('.column-seventh');
 var $span = document.querySelector('span');
 
+var data = {
+  sunday: {
+    entries: []
+  },
+  monday: {
+    entries: []
+  },
+  tuesday: {
+    entries: []
+  },
+  wednesday: {
+    entries: []
+  },
+  thursday: {
+    entries: []
+  },
+  friday: {
+    entries: []
+  },
+  saturday: {
+    entries: []
+  }
+};
+
+var previousEntryDataJSON = localStorage.getItem('entry-data');
+
+if (previousEntryDataJSON !== null) {
+  data = JSON.parse(previousEntryDataJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+
+  localStorage.setItem('entry-data', dataJSON);
+});
+
 $addEntry.addEventListener('click', function (event) {
   $modalContainer.className = 'modal-container';
 });
 
 $addEntryForm.addEventListener('submit', function (event) {
   event.preventDefault();
+
+  var newEntry = {
+    time: document.querySelector('#time').value,
+    description: document.querySelector('#description').value
+  };
+
+  for (var key in data) {
+    if (data[key] === document.querySelector('#day-of-week').value) {
+      data[key].entries.push(newEntry);
+    }
+  }
 
   $modalContainer.className = 'modal-conatiner hidden';
 });
